8176f678a995fd5b031e4eb60585984b6a25d59c
GAURAV <gaauuuraavvv@gmail.com>
2025-07-26 09:01:43 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export {default as Transactions} from './Transactions';
+export {default as Navbar} from './Navbar';
+export {default as Services} from './Services';
+export {default as Welcome} from './Welcome';
+
